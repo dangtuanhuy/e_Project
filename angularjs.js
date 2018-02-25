8caaa1e6f21b99e113c3dc9b09407d0724c99d2a
Huy <huyb1505883@student.ctu.edu.vn>
2018-02-25 22:46:18 +0700

    Đưa mã nguồn lên

---
+++ angularjs.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+app.controller('MyController', function($scope) {
+	$scope.studentinfo = [
+		{name: 'Dang Tuan Huy', old: '21', item: 'images/DangTuanHuy.png'},
+		{name: 'Lu Minh Thong', old: '18', item: 'images/LuMinhThong.jpg'},
+		{name: 'Truong Phu Ngan', old: '18', item: 'images/TruongPhuNgan.jpg'}
+	];
+});
