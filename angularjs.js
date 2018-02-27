fbf8c75b2aabc91882caaefa8fe22c6a617c3739
Huy <huyb1505883@student.ctu.edu.vn>
2018-02-27 20:35:29 +0700

    GEO

--- angularjs.js
+++ angularjs.js
@@ -1,8 +1,8 @@
 var app = angular.module('myApp', []);
 app.controller('MyController', function($scope) {
 	$scope.studentinfo = [
-		{name: 'Dang Tuan Huy', old: '21', item: 'images/DangTuanHuy.png'},
-		{name: 'Lu Minh Thong', old: '18', item: 'images/LuMinhThong.jpg'},
-		{name: 'Truong Phu Ngan', old: '18', item: 'images/TruongPhuNgan.jpg'}
+		{name: 'Dang Tuan Huy', old: '21', item: 'images/DangTuanHuy.png', mssv: 'A17007'},
+		{name: 'Lu Minh Thong', old: '18', item: 'images/LuMinhThong.jpg',mssv: 'A17006'},
+		{name: 'Truong Phu Ngan', old: '18', item: 'images/TruongPhuNgan.jpg',mssv: 'A17005'}
 	];
 });
